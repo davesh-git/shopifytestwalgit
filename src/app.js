68c222e5757f3355aad43bc2c677eb1dd4c7a0b0
Davesh Bhambey <davesh4all@gmail.com>
2019-06-04 16:42:18 -0700

    app update to handle app scenarios

--- src/app.js
+++ src/app.js
@@ -29,8 +29,7 @@ app.get('', (req, res) => {
 
     //STEP 2
     //Validate HMAC - Take the entire string value (Except hmac code) and process through HMAC algo to get hexdigest and match it with hmac value to validate
-
-    console.log(chalk.green("Received Call from Shopify"))
+    console.log(chalk.green("---------------Received Installation Call from Shopify------------------"))
     const shopName = 'test-wal-mp'// req.query.shop
     const urlParams = new URLSearchParams(req.query)
 
@@ -44,8 +43,6 @@ app.get('', (req, res) => {
 
             //URL the Shop gets redirected to post installation requests. Note : If a shop logins again after installation, shopify first calls '/' and then gets redirected to '/welcome'
             const firstWelcomeRedirectURL = envVarUtil.envVars.APP_SERV + '/welcome'
-            const secondWelcomeRedirectURL = envVarUtil.envVars.APP_SERV + '/homepage'
-
             const shopScope = shopAccessUtil.shopAccess
             //Randomly Generated value(store it for auth callback to show welcome screen)
             const shopNonce = 1234353590590904
@@ -53,6 +50,8 @@ app.get('', (req, res) => {
             const shopAccessMode = ''
 
             let shopsParseObj = ''
+
+            console.log(chalk.green("Parameters initialized"))
 
             try {
                 //Get data to check further if shop exists
@@ -70,8 +69,8 @@ app.get('', (req, res) => {
 
             //Default value to send to installation prompt with /welcome URL
             let redirectInstallFlag = true
-            //TRUE - Shop DOES NOT exists - Redirect to hopify Install Prompt 
-            //FALSE - Shop Exists, and send to /homepage URL
+            //TRUE - Shop DOES NOT exists - Redirect to Shopify Install Prompt 
+            //FALSE - Shop Exists, and reject the request
 
             //STEP 3 
             //If some data is found, then shop may or may not exist
@@ -81,13 +80,11 @@ app.get('', (req, res) => {
                 if (shopsParseObj.filter((shop) => shop.Shop_Name === 'test-wal-mp')) {
                     //This is the redirect URL after installation. In the post calls, that is after app installations, this '/' will be called but we might not have to pass all these parameters (check SLDB and change the Params)
                     //Also, when User comes to '/welcome' it should be dynamic content
-                    redirectURL = 'https://' + encodeURIComponent(shopName) + '.myshopify.com/admin/oauth/authorize?client_id=' + encodeURIComponent(envVarUtil.envVars.SHOPIFY_API_KEY) +
-                        '&scope=' + encodeURIComponent(shopScope) + '&redirect_uri=' + secondWelcomeRedirectURL + '&state=' + shopNonce + '&grant_options[]=' + shopAccessMode
-                    consoleStr = 'This is the second time post-installation call'
+                    //  redirectURL = 'https://' + encodeURIComponent(shopName) + '.myshopify.com/admin/oauth/authorize?client_id=' + encodeURIComponent(envVarUtil.envVars.SHOPIFY_API_KEY) +
+                    //     '&scope=' + encodeURIComponent(shopScope) + '&redirect_uri=' + secondWelcomeRedirectURL + '&state=' + shopNonce + '&grant_options[]=' + shopAccessMode
+                    consoleStr = 'ALERT! -----  Invalid call and security risk. '
                     redirectInstallFlag = false
-
                 }
-
             }
 
             //FIRST TIME shop has installed the app, or somehow data does not exist at our end 
@@ -95,12 +92,10 @@ app.get('', (req, res) => {
                 //Redirect to Shopify Install Prompt - THIS IS THE FIRST TIME USER HAS NOT INSTALLED THE APP (Check in our DB config - SLDB lookup)
                 redirectURL = 'https://' + encodeURIComponent(shopName) + '.myshopify.com/admin/oauth/authorize?client_id=' + encodeURIComponent(envVarUtil.envVars.SHOPIFY_API_KEY) +
                     '&scope=' + encodeURIComponent(shopScope) + '&redirect_uri=' + firstWelcomeRedirectURL + '&state=' + shopNonce + '&grant_options[]=' + shopAccessMode
-
                 consoleStr = 'This is the first time installation call'
             }
 
-
-            console.log(consoleStr + ':' + req.url + ',query:' + req.query)
+            console.log(chalk.green(consoleStr + ':' + req.url + ',query:' + req.query))
             console.log(chalk.yellow('Redirecting to : ' + redirectURL))
 
             //Redirect call to shopify
@@ -108,13 +103,13 @@ app.get('', (req, res) => {
                 { Location: redirectURL }
             );
             res.end();
-            console.log("END OF CALL")
+            console.log(chalk.green('END OF CALL'))
 
         }
 
         //Request invalidated - Reject
         else {
-            //DO NOTHING or SHOw ERROR WEBPAGE
+            //DO NOTHING or SHOW ERROR WEBPAGE
         }
     })
 })
@@ -122,7 +117,7 @@ app.get('', (req, res) => {
 //This is  called after the user has confirmed installation and gets redirected to the welcome screen. At this stage we will fetch the tokens and persist in our DB i.e Appvault
 app.get('/welcome', (req, res) => {
 
-    console.log('Shopify second call from app just after installation' + req.url + ",query:" + req.query)
+    console.log(chalk.green('------------------------Shopify second call from app just after installation' + req.url + ",query:" + req.query + '----------------------'))
 
     //STEP 4 VALIDATE
     // The nonce is the same one that your app provided to Shopify during step two (to make suree this was redirected call from '/')
@@ -149,6 +144,16 @@ app.get('/welcome', (req, res) => {
             let consoleStr = ''
             let shopFoundFlag = true
 
+            //Validate Nonce code
+            const nonce2 = queryURLParts.get('nonce')
+            if (nonce2 === 1234353590590904) {
+                console.log(chalk.green('Nonce validated!!!'))
+            }
+            else {
+                console.log(chalk.red('ALERT! - NONCE INVALID'))
+            }
+
+
             //After install - if triggered by mistake and shop already exists, show invalid message
 
             //Some Shop data exists, and specific shop info may or may not exist
@@ -158,51 +163,69 @@ app.get('/welcome', (req, res) => {
                 if (shopsParseObj.filter((shop) => shop.Shop_Name === 'test-wal-mp')) {
                     shopFoundFlag = true
 
+                    console.log(chalk.red('ALERT! - This call is not possible and RISK'))
                     //Shop found and redirect to invalid message as /welcome static message only first time after installation
                     return res.send('Invalid Redirection, please try again')
                 }
             }
 
-            //If shop was not found, and this was the first time /welcome call
+            //If shop was not found, and this was the valid first time /welcome call
             if (shopFoundFlag === true) {
                 const shopJSON = [{
                     Shop_Name: 'test-wal-mp'
                 }]
+                console.log(chalk.green('Adding to DB that partner has installed the app'))
                 const shopStr = JSON.stringify(shopJSON)
                 fs.writeFileSync(shopFilePath, shopStr)
 
                 debugger
+
                 //GET THE PERMANENT TOKEN FOR BACKEND CALLS  (STORE IT IN KEYSTORE DB) -CHECK ALL REQUESTED SCOPES ARE GIVEN (only write ones will be sent back)
                 //(shopName, client_id, client_secret, authcode, callback) 
-                getTokenUtil.getToken('test-wal-mp', envVarUtil.envVars.SHOPIFY_API_KEY, envVarUtil.envVars.SHOPIFY_SECRET_API_KEY, req.query.code, (response) => {
-                    if (response.error !== undefined) {
-                        console.log(response.token + ',' + response.scope)
-                    }
-                    else {
-                        console.log(chalk.red(response.error))
-                    }
-
+                let tokenCall = getTokenUtil.getToken('test-wal-mp', envVarUtil.envVars.SHOPIFY_API_KEY, envVarUtil.envVars.SHOPIFY_API_SECRET_KEY, req.query.code)
+                tokenCall.then((response) => {
+                    console.log(chalk.yellow(response.token + ',' + response.scope))
+                }, (error) => {
+                    console.log(chalk.red(response.error))
                 })
+
+                // //GET THE PERMANENT TOKEN FOR BACKEND CALLS  (STORE IT IN KEYSTORE DB) -CHECK ALL REQUESTED SCOPES ARE GIVEN (only write ones will be sent back)
+                // //(shopName, client_id, client_secret, authcode, callback) 
+                // getTokenUtil.getToken('test-wal-mp', envVarUtil.envVars.SHOPIFY_API_KEY, envVarUtil.envVars.SHOPIFY_SECRET_API_KEY, req.query.code, (response) => {
+
+                //     console.log(chalk.green('Call to Shopify to fetch API token'))
+
+                //     if (response.error !== undefined) {
+                //         console.log(chalk.yellow(response.token + ',' + response.scope))
+
+                //         //SAVE THE TOKEN IN DB
+                //     }
+                //     else {
+                //         console.log(chalk.red(response.error))
+                //     }
+
+                //})
 
                 //Render welcome page
                 return res.render('welcome.hbs')
             }
         }
         else {
-            //DO NOTHING or SHOw ERROR WEBPAGE
+            //DO NOTHING or SHOW ERROR WEBPAGE
         }
 
     })
 })
 
 
-//This is called once user opens the installed app and redirected from '/' 
+//This is called once user opens the installed app and first navigation link
 app.get('/homepage', (req, res) => {
 
-    console.log('Shopify second call from app (Already installed)' + req.url + ",query:" + req.query)
+    console.log(chalk.green('-----------------Shopify Home page request from app (Already installed)' + req.url + ",query:" + req.query + '-------------------'))
+
+    console.log(chalk.yellow('URL called should have nonce:' + req.query.url))
 
     //STEP 4 VALIDATE
-    // The nonce is the same one that your app provided to Shopify during step two (to make suree this was redirected call from '/')
     // The hmac is valid. The HMAC is signed by Shopify as explained below, in Verification.
     // The hostname parameter is a valid hostname, ends with myshopify.com, and does not contain characters other than letters (a-z), numbers (0-9), dots, and hyphens.
     const urlParams = new URLSearchParams(req.query)
@@ -230,10 +253,11 @@ app.get('/homepage', (req, res) => {
 
                 if (shopsParseObj.filter((shop) => shop.Shop_Name === 'test-wal-mp')) {
 
-                    //GET THE ONLINE ACCESS TOKEN FOR UI CALLS AND AUTHENTICATION WHICH USER IS USING THIS APP
+                    //GET THE ONLINE ACCESS TOKEN FOR UI CALLS AND AUTHENTICATION WHICH USER IS USING THIS APP (This should only happen if token fetch during welcome page failed)
 
                     //Redirect to homepage
                     debugger
+                    //-----IF TOKEN DOES NOT EXIST ALREADY---s)
                     //GET THE PERMANENT TOKEN FOR BACKEND CALLS  (STORE IT IN KEYSTORE DB) -CHECK ALL REQUESTED SCOPES ARE GIVEN (only write ones will be sent back)
                     //(shopName, client_id, client_secret, authcode, callback) 
                     let tokenCall = getTokenUtil.getToken('test-wal-mp', envVarUtil.envVars.SHOPIFY_API_KEY, envVarUtil.envVars.SHOPIFY_API_SECRET_KEY, req.query.code)
@@ -260,11 +284,11 @@ app.get('/homepage', (req, res) => {
 
 })
 
-app.get('/testpage',(req,res)=>{
+app.get('/testpage', (req, res) => {
     res.send('Test page is working, Thanks')
 })
 
-app.get('/syncproducts',(req,res)=>{
+app.get('/syncproducts', (req, res) => {
 
     //STEP 4 VALIDATE
     // The nonce is the same one that your app provided to Shopify during step two (to make suree this was redirected call from '/')
@@ -311,8 +335,8 @@ app.get('/syncproducts',(req,res)=>{
 })
 
 
-app.get('/manageproducts',(req,res)=>{
- //STEP 4 VALIDATE
+app.get('/manageproducts', (req, res) => {
+    //STEP 4 VALIDATE
     // The nonce is the same one that your app provided to Shopify during step two (to make suree this was redirected call from '/')
     // The hmac is valid. The HMAC is signed by Shopify as explained below, in Verification.
     // The hostname parameter is a valid hostname, ends with myshopify.com, and does not contain characters other than letters (a-z), numbers (0-9), dots, and hyphens.
@@ -357,7 +381,7 @@ app.get('/manageproducts',(req,res)=>{
 })
 
 
-app.listen(process.env.PORT || 3000),()=>{
+app.listen(process.env.PORT || 3000), () => {
     console.log("Server is runnxing on port 3000")
 }
 
